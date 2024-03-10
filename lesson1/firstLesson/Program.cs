82f70918b4e665f80da75e50a83937c747c16121
unknown <ieseanuveaceslav32@gmail.com>
2024-03-10 16:00:43 +0200

    primalectie

---
+++ lesson1/firstLesson/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace firstLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            float test = 5.66F;
+            double test2 = 5.99D;
+            int test3 = 5;
+            bool test4 = true;
+            char test5 = 'a';
+            string test6  = "b";
+        }
+    }
+}
